4c3f6171bf10d55eb25209c15cbdd573cb75adbe
Timm Friebe <thekid@thekid.de>
2015-01-03 15:01:51 +0100

    QA: Simplify code a bit

--- src/main/php/util/invoke/RateLimiting.class.php
+++ src/main/php/util/invoke/RateLimiting.class.php
@@ -89,10 +89,10 @@ class RateLimiting extends \lang\Object {
     $this->permits+= $permits;
 
     // If we reached (or exceeded) the rate, set next slot to the future
-    if ($this->permits === $this->rate->value()) {
+    $exceed= $this->permits - $this->rate->value();
+    if (0 === $exceed) {
       $this->next= $time + $this->rate->unit()->seconds();
     } else if ($this->permits > $this->rate->value()) {
-      $exceed= $this->permits - $this->rate->value();
       $buffers= (int)ceil($exceed / $this->rate->value());
       $this->next= $time + $buffers * $this->rate->unit()->seconds();
     }
